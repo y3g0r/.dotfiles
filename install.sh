67b3c9e99322413699f9e6cce24645cc60cfeec4
Yegor Ivashchenko <to.yegor@gmail.com>
2016-09-17 12:29:41 +0300

    Fix install.sh, rename dotfiles

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-ln -sv "$HOME/.dotfiles/.vimrc" $HOME
+#!/usr/bin/env bash
+# directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# vimrc
+ln -sv "$DIR/vimrc" $HOME/.vimrc
